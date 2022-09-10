f41f7c830d7093e03f8abd8d2915f4522816e32c
duc23 <30811163+duc23@users.noreply.github.com>
2022-09-10 00:35:43 -0700

    Time: 6922 ms (8.94%), Space: 14.9 MB (76.28%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-                 # loop through all #'s
+            # loop through all #'s
         for i in range(len(nums)):
             # loop through all #'s starting with the next index over
             for j in range(i+1, len(nums)):
