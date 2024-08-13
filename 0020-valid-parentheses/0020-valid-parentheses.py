ed5e710c87451a3fe8772a186ab4531463888ee9
duc23 <30811163+duc23@users.noreply.github.com>
2024-08-12 23:52:23 -0700

    Time: 34 ms (67.74%), Space: 16.5 MB (88.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,14 +2,14 @@ class Solution:
     def isValid(self, s: str) -> bool:
         # build stack as empty list
         stack = []
-        # dictionary to match the different parenthesis type with its open parenthesis
+        # dictionary/hashmap to match the different parenthesis type with its open parenthesis
         Map = {")":"(", "]":"[", "}":"{"}
         # for every parenthesis in our string
         for parenthesis in s:
             # if it's an open parenthesis, add to stack
             if parenthesis in Map.values():
                 stack.append(parenthesis)
-            # else it's a closed bracket, and make sure the type are the same at the top of the stack. If so, pop it off the stack since they match.
+            # else it's a closed bracket, and make sure the type is the same at the top of the stack. If so, pop it off the stack since they match.
             elif stack and Map[parenthesis] == stack[-1]:
                 stack.pop()
             # if there's no stack and they dont match, it's invalid
